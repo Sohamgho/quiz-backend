aaa8cb574e9c35637be2ba4b11f1b6d1119813a6
Sohamgho <sohamghosh177@gmail.com>
2025-11-20 08:29:15 +0530

    initial commit

--- src/main/java/com/wbjee/oceanapp/OceanappApplication.java
+++ src/main/java/com/wbjee/oceanapp/OceanappApplication.java
@@ -3,6 +3,8 @@ package com.wbjee.oceanapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class OceanappApplication {
 
